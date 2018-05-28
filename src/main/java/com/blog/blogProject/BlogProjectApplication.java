9ee985b24fcacf2b3d165ba2a70b1974b3f5cef2
hujwala <upatel@qwinixtech.com>
2018-05-28 15:31:06 +0530

    first commit

---
+++ src/main/java/com/blog/blogProject/BlogProjectApplication.java
@@ -0,0 +1,12 @@
+package com.blog.blogProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogProjectApplication.class, args);
+	}
+}
