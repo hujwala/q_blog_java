c8f977e66bea9d4b68eb7f33c0e833d2220097a5
Pradeep <pradhi.sjv@gmail.com>
2018-05-28 16:35:54 +0530

    Added signup APi

---
+++ src/main/java/Repo/UserRepo.java
@@ -0,0 +1,7 @@
+package Repo;
+
+import model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo extends CrudRepository<User,String> {
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,88 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class User {
+
+
+    private String name;
+    @Id
+    private String email;
+    private String password;
+    private String profileImage;
+    private int writtenStoryCount;
+    private Date createedAt;
+    private Date updatedAt;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, String profileImage, int writtenStoryCount, Date createedAt, Date updatedAt) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.profileImage = profileImage;
+        this.writtenStoryCount = writtenStoryCount;
+        this.createedAt = createedAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public int getWrittenStoryCount() {
+        return writtenStoryCount;
+    }
+
+    public void setWrittenStoryCount(int writtenStoryCount) {
+        this.writtenStoryCount = writtenStoryCount;
+    }
+
+    public Date getCreateedAt() {
+        return createedAt;
+    }
+
+    public void setCreateedAt(Date createedAt) {
+        this.createedAt = createedAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
