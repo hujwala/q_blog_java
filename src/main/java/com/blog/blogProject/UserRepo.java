d53dca297ea5ee8d59b576e7e49fa13d3648d199
hujwala <upatel@qwinixtech.com>
2018-05-30 17:17:22 +0530

    deleted extra file

--- src/main/java/com/blog/blogProject/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.blog.blogProject;
-
-import com.blog.blogProject.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepo extends CrudRepository<User,String> {
-}
